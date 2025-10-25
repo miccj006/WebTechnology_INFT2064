05fc432bbecd7484b2102827a3a874e84648f8ff
miccj006 <miccj006@mymail.unisa.edu.au>
2025-10-25 21:29:47 +1030

    Week 11 - Activity
    Created a react app and implemented cards.

---
+++ AOWebApp/aowebapp/aowebapp/src/components/Card.js
@@ -0,0 +1,15 @@
+﻿function Card(props) {
+    return (
+        <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }} >
+            <img className="card-img-top" src={props.itemImage} alt={"Image of " + props.itemName} />
+            <div className="card-body">
+                <h5 className="card-title">{props.itemName}</h5>
+                <p className="card-text">{props.itemDescription}</p>
+                <p className="card-text">${props.itemCost}</p>
+                <a hrel="#" className="btn btn-primary">Go somewhere {props.itemId}</a>
+            </div>
+        </div>
+    )
+}
+
+export default Card
